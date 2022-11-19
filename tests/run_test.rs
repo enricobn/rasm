b219e4ab76dfebe63a439c4ceb65193714f118fb
Enrico Benedetti <enrico_benedetti@yahoo.com>
2022-11-19 18:20:47 +0100

    optimized cprint avoiding slen

--- tests/run_test.rs
+++ tests/run_test.rs
@@ -196,7 +196,7 @@ fn test_lines() {
 
 #[test]
 fn test_chars() {
-    test("chars", Vec::new(), "😀\ntrue\nfalse\nH,e,l,l,o\n");
+    test("chars", Vec::new(), "a\nè\n😀\ntrue\nfalse\nH,e,l,l,o\n");
 }
 
 #[test]
