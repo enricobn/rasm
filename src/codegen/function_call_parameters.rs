enricobn/rasm

commit c48d107b66b99c3152743cdaddcbe360c857b158

fixed bug when a parameter name is the start of another parameter name, i.e. a a1

src/codegen/function_call_parameters.rs
@@ -582,9 +582,14 @@ impl<'a> FunctionCallParameters<'a> {
     ) -> String {
         let mut result = body.to_string();
 
-        let mut i = 0;
         let word_len = self.backend.word_len() as i32;
-        for par in self.parameters.iter() {
+
+        let mut parameters = self.parameters.iter().enumerate().collect::<Vec<_>>();
+        parameters.sort_by(|(_, a), (_, b)| a.name.cmp(&b.name).reverse());
+
+        for (index, par) in parameters.iter() {
+            let i = *index;
+
             if let Some(par_value) = self.parameters_values.get(&par.name) {
                 debug!(
                     "{}found parameter {}, value: {}",
@@ -632,7 +637,7 @@ impl<'a> FunctionCallParameters<'a> {
                     crate::codegen::STACK_VAL_SIZE_NAME
                 )
             } else {
-                format!("{}", (i + 2) * self.backend.word_len() as i32)
+                format!("{}", (i as i32 + 2) * self.backend.word_len() as i32)
             };
 
             let address = format!(
@@ -642,7 +647,6 @@ impl<'a> FunctionCallParameters<'a> {
             );
 
             result = result.replace(&format!("${}", par.name), &address);
-            i += 1;
         }
 
         result

commit 2d765f143dc24e1a5c3adb97a544e30e741c06ad

fixed bug introduced with rev c48d107b66b99c3152743cdaddcbe360c857b158

src/codegen/function_call_parameters.rs
@@ -584,12 +584,11 @@ impl<'a> FunctionCallParameters<'a> {
 
         let word_len = self.backend.word_len() as i32;
 
-        let mut parameters = self.parameters.iter().enumerate().collect::<Vec<_>>();
-        parameters.sort_by(|(_, a), (_, b)| a.name.cmp(&b.name).reverse());
+        let mut i = 0;
 
-        for (index, par) in parameters.iter() {
-            let i = *index;
+        let mut substitutions = Vec::new();
 
+        for par in self.parameters.iter() {
             if let Some(par_value) = self.parameters_values.get(&par.name) {
                 debug!(
                     "{}found parameter {}, value: {}",
@@ -601,7 +600,7 @@ impl<'a> FunctionCallParameters<'a> {
                     ";found parameter {}, value: {}\n",
                     par.name, par_value
                 ));
-                result = result.replace(&format!("${}", par.name), par_value);
+                substitutions.push((par.name.clone(), par_value.clone()));
                 continue;
             }
 
@@ -646,7 +645,14 @@ impl<'a> FunctionCallParameters<'a> {
                 relative_address
             );
 
-            result = result.replace(&format!("${}", par.name), &address);
+            substitutions.push((par.name.clone(), address));
+            i += 1;
+        }
+
+        substitutions.sort_by(|(a, _), (b, _)| a.cmp(b).reverse());
+
+        for (par_name, value) in substitutions {
+            result = result.replace(&format!("${}", par_name), &value);
         }
 
         result