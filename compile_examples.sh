a769ccf9f0be6dc97a26208d777ba2f82d1d7822
Enrico Benedetti <enrico_benedetti@yahoo.com>
2023-07-25 04:39:43 +0200

    more on bouncing_quads example

--- compile_examples.sh
+++ compile_examples.sh
@@ -5,3 +5,4 @@ time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/exampl
 time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/gameoflife_vec.rasm gameoflife_vec
 time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/gameoflife_vec_sdl.rasm gameoflife_vec_sdl
 time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/showimage.rasm showimage
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/bouncing_quads.rasm bouncing_quads
