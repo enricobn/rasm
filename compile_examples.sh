e730f9e2139a96d29247eb1f3f92708cff4bf49a
Enrico Benedetti <enrico_benedetti@yahoo.com>
2023-10-27 13:57:16 +0200

    fixed compile_examples.sh

--- compile_examples.sh
+++ compile_examples.sh
@@ -1,9 +1,9 @@
 set -e
-time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/breakout breakout.out
-time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/gameoflife.rasm gameoflife.out
-time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/gameoflife_sdl.rasm gameoflife_sdl.out
-time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/gameoflife_vec.rasm gameoflife_vec.out
-time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/gameoflife_vec_sdl.rasm gameoflife_vec_sdl.out
-time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/showimage.rasm showimage.out
-time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/bouncing_quads.rasm bouncing_quads.out
-time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/test/fibonacci.rasm fibonacci.out
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/breakout breakout
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/gameoflife.rasm gameoflife
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/gameoflife_sdl.rasm gameoflife_sdl
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/gameoflife_vec.rasm gameoflife_vec
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/gameoflife_vec_sdl.rasm gameoflife_vec_sdl
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/showimage.rasm showimage
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/examples/bouncing_quads.rasm bouncing_quads
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release rasm/resources/test/fibonacci.rasm fibonacci
