96c80016cbc0f2bede19e1694ccd845b25d9436e
Enrico Benedetti <enrico_benedetti@yahoo.com>
2024-06-28 18:43:50 +0200

    C backend: optional argument to compile_examples_c.sh

--- compile_examples_c.sh
+++ compile_examples_c.sh
@@ -1,9 +1,9 @@
 set -e
-time RUST_LOG=info RUST_BACKTRACE=full cargo run --release build -t c rasm/resources/examples/breakout -o tmp/breakout
-time RUST_LOG=info RUST_BACKTRACE=full cargo run --release build -t c rasm/resources/examples/gameoflife -o tmp/gameoflife
-time RUST_LOG=info RUST_BACKTRACE=full cargo run --release build -t c rasm/resources/examples/gameoflife_sdl -o tmp/gameoflife_sdl
-time RUST_LOG=info RUST_BACKTRACE=full cargo run --release build -t c rasm/resources/examples/gameoflife_vec -o tmp/gameoflife_vec
-time RUST_LOG=info RUST_BACKTRACE=full cargo run --release build -t c rasm/resources/examples/gameoflife_vec_sdl -o tmp/gameoflife_vec_sdl
-time RUST_LOG=info RUST_BACKTRACE=full cargo run --release build -t c rasm/resources/examples/showimage -o tmp/showimage
-time RUST_LOG=info RUST_BACKTRACE=full cargo run --release build -t c rasm/resources/examples/bouncing_quads -o tmp/bouncing_quads
-time RUST_LOG=info RUST_BACKTRACE=full cargo run --release build -t c rasm/resources/test/fibonacci.rasm -o tmp/fibonacci
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release build -t c rasm/resources/examples/breakout -o tmp/breakout $1
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release build -t c rasm/resources/examples/gameoflife -o tmp/gameoflife $1
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release build -t c rasm/resources/examples/gameoflife_sdl -o tmp/gameoflife_sdl $1
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release build -t c rasm/resources/examples/gameoflife_vec -o tmp/gameoflife_vec $1
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release build -t c rasm/resources/examples/gameoflife_vec_sdl -o tmp/gameoflife_vec_sdl $1
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release build -t c rasm/resources/examples/showimage -o tmp/showimage $1
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release build -t c rasm/resources/examples/bouncing_quads -o tmp/bouncing_quads $1
+time RUST_LOG=info RUST_BACKTRACE=full cargo run --release build -t c rasm/resources/test/fibonacci.rasm -o tmp/fibonacci $1
