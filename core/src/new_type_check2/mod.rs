b9cf720e156934d8865864a4a9dc57a4fb21ad29
Enrico Benedetti <enrico_benedetti@yahoo.com>
2024-08-24 09:33:02 +0200

    fixedunique_type_name

--- core/src/new_type_check2/mod.rs
+++ core/src/new_type_check2/mod.rs
@@ -520,12 +520,12 @@ impl TypeCheck {
                     return_type,
                 } => {
                     format!(
-                        "fn{}{}",
+                        "fn_{}_{}",
                         parameters
                             .iter()
                             .map(Self::unique_type_name)
                             .collect::<Vec<_>>()
-                            .join(""),
+                            .join("_"),
                         Self::unique_type_name(return_type)
                     )
                 }
@@ -539,13 +539,12 @@ impl TypeCheck {
                 index: _,
             } => {
                 format!(
-                    "{}{name}{}",
-                    namespace.safe_name(),
+                    "{name}_{}",
                     param_types
                         .iter()
                         .map(Self::unique_type_name)
                         .collect::<Vec<_>>()
-                        .join("")
+                        .join("_")
                 )
             }
             ASTType::Unit => "Unit".to_string(),
