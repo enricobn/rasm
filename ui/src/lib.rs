04639b2768c88be7160cba4230155ae93590b44a
Enrico Benedetti <enrico_benedetti@yahoo.com>
2024-09-21 14:44:22 +0200

    ui functions

--- ui/src/lib.rs
+++ ui/src/lib.rs
@@ -1,18 +1,23 @@
 pub struct UI {
     project: RasmProject,
     current_module: Option<String>,
+    modules: Vec<String>,
+    target: CompileTarget,
+    current_function: Option<ASTFunctionDef>,
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
-    Open(String),
+    Module(String),
+    Function(ASTFunctionDef),
+    BackToModule,
     Home,
 }
 
-use std::env;
+use std::{env, path::Path};
 
 use iced::{
-    widget::{button, column, text, Column, Row, Scrollable},
+    widget::{button, column, keyed::column, text, Column, Row, Scrollable},
     Element, Task,
 };
 use rasm_core::{
@@ -21,28 +26,16 @@ use rasm_core::{
         statics::Statics,
     },
     commandline::CommandLineOptions,
-    parser::ast::ASTModule,
+    parser::ast::{ASTFunctionDef, ASTModule},
     project::RasmProject,
 };
 
 impl UI {
     pub fn show(project: RasmProject) -> iced::Result {
-        iced::application("Rasm project UI", UI::update, UI::view).run_with(|| {
-            (
-                UI {
-                    project,
-                    current_module: None,
-                },
-                Task::none(),
-            )
-        })
-    }
-
-    pub fn view(&self) -> Element<Message> {
         let mut statics = Statics::new();
         let target = CompileTarget::C(COptions::default());
 
-        let (modules, _errors) = self.project.get_all_modules(
+        let (modules, _errors) = project.get_all_modules(
             &mut statics,
             false,
             &target,
@@ -51,37 +44,57 @@ impl UI {
             &CommandLineOptions::default(),
         );
 
-        match &self.current_module {
-            Some(s) => self.module(s),
-            None => self.home(modules),
+        iced::application("Rasm project UI", UI::update, UI::view).run_with(|| {
+            (
+                UI {
+                    project,
+                    modules: modules
+                        .into_iter()
+                        .map(|it| it.path.to_string_lossy().to_owned().to_string())
+                        .collect(),
+                    target,
+                    current_module: None,
+                    current_function: None,
+                },
+                Task::none(),
+            )
+        })
+    }
+
+    pub fn view(&self) -> Element<Message> {
+        match &self.current_function {
+            Some(function) => self.show_function(function),
+            None => match &self.current_module {
+                Some(s) => self.show_module(s),
+                None => self.home(),
+            },
         }
 
         /*
         let (enhanced_ast_module, _errors) =
             EnhancedASTModule::new(modules, &self.project, &mut statics, &target, false);
             */
-
-        //Scrollable::new(column).into()
     }
 
     pub fn update(&mut self, message: Message) {
         match message {
-            Message::Open(s) => self.current_module = Some(s),
-            Message::Home => self.current_module = None,
+            Message::Home => {
+                self.current_module = None;
+                self.current_function = None
+            }
+            Message::Module(s) => self.current_module = Some(s),
+            Message::Function(function) => self.current_function = Some(function),
+            Message::BackToModule => self.current_function = None,
         }
     }
 
-    fn module<'a>(&'a self, module_path: &'a str) -> Element<Message> {
-        column!(button("Back").on_press(Message::Home), text(module_path)).into()
-    }
-
-    fn home(&self, modules: Vec<ASTModule>) -> Element<Message> {
+    fn home(&self) -> Element<Message> {
         let mut column = Column::new();
-        for module in modules.iter() {
-            let t = module.path.to_string_lossy().to_owned().to_string();
+        for module in self.modules.iter() {
             let row = Row::new()
-                .push(button("open").on_press(Message::Open(t.clone())))
-                .push(text(t));
+                .spacing(10)
+                .push(button("open").on_press(Message::Module(module.clone())))
+                .push(text(module));
             column = column.push(row);
         }
 
@@ -91,5 +104,34 @@ impl UI {
             */
 
         Scrollable::new(column).into()
+    }
+
+    fn show_module<'a>(&'a self, module_path: &'a str) -> Element<Message> {
+        let mut column = Column::new()
+            .spacing(10)
+            .push(button("Back").on_press(Message::Home))
+            .push(text(module_path));
+        if let Some((module, _errors)) = self
+            .project
+            .get_module(Path::new(module_path), &self.target)
+        {
+            for function in module.functions.iter() {
+                let row = Row::new()
+                    .spacing(10)
+                    .push(button("show").on_press(Message::Function(function.clone())))
+                    .push(text(format!("{}", function)));
+                column = column.push(row);
+            }
+        }
+        column.into()
+    }
+
+    fn show_function<'a>(&'a self, function: &'a ASTFunctionDef) -> Element<Message> {
+        let column = Column::new()
+            .spacing(10)
+            .push(button("Back").on_press(Message::BackToModule))
+            .push(text(format!("{}", function)));
+
+        column.into()
     }
 }
