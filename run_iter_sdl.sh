8ff923bd2d9afee00f21a1b296a19a13088083a6
Enrico Benedetti <enrico_benedetti@yahoo.com>
2025-10-11 09:15:09 +0200

    fixed example gameoflife_iter_sdl

--- run_iter_sdl.sh
+++ run_iter_sdl.sh
@@ -1,6 +1,6 @@
 cargo build --release
 rm -r tmp
 mkdir tmp
-RUST_LOG=info RUST_BACKTRACE=full target/release/rasm build rasm/resources/examples/gameoflife_iter_sdl -t c -o tmp/gameoflife_iter_sdl $1
+RUST_LOG=info RUST_BACKTRACE=full target/release/rasm build rasm/resources/examples/gameoflife_iter_sdl -t c -o tmp $1
 echo "run..."
 time tmp/gameoflife_iter_sdl rasm/resources/examples/f116.cells 3
